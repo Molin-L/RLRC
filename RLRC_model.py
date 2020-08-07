e1c7c3f77c93d17b56a98fdabd445e8f9971c352
Molin, L <2473686l@student.gla.ac.uk>
2020-08-07 22:17:22 +0100

    Update original data

--- RLRC_model.py
+++ RLRC_model.py
@@ -18,7 +18,15 @@ additional_special_tokens = ['[E1]', '[/E1]', '[E2]', '[/E2]']
 
 
 class RC_CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, config):
+        super(RC_CNN, self).__init__()
+        self.config = config
+        self.conv1 = nn.Conv2d(1, 1, kernel_size=[3, 60], stride=[1, 60])
+        self.pool = nn.MaxPool2d(kernel_size=[70, 1], stride=1)
+        self.drop = nn.Dropout(p=config.keep_prob)
+        self.fc1 = nn.Linear(self.config.cnn_size, self.num_classes)
+
+    def forward(self, x):
         pass
 
     def save(self, save_dir='./models'):
